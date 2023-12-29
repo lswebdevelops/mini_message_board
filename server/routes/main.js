9c9c72973dc9db760ce6792e5c96909fb12ae874
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-12-29 13:40:11 -0300

    adding gradient

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("hi lou");
+
+    const locals = {
+        title: 'Mini Messageboard',
+        description: "Simple Messageboard created with NojeJs, Express & MongoDb."
+    }
+
+  res.render("index", locals);
+});
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 module.exports = router;
